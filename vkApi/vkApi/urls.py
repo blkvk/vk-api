0e23f66f50910c0a479f7f4f16ea4ba09b5acd9f
Kirill Beliakov <kabeliakov@gmail.com>
2022-04-19 14:50:26 +0300

    f

--- vkApi/vkApi/urls.py
+++ vkApi/vkApi/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/asd', admin.site.urls),
     path('', include('mari.urls'))
 ]
